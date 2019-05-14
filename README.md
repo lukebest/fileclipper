01e834b7698c2a3eb684f08c26e802a2c8d88927
Luke Liu <liuyingxyzabc@gmail.com>
2019-05-14 09:55:32 +0800

    Use new algorithgm to find rect coutour

--- README.md
+++ README.md
@@ -8,3 +8,14 @@
 ![识别后](https://github.com/Wangzg123/fileclipper/blob/master/result.jpg?raw=true)
 
 详细可以参考博客 https://blog.csdn.net/qq8993174/article/details/89785887
+
+接口调用方法：
+```python
+from skimage_clipper import main_img_preprocess
+from skimage import io
+
+origin_img = io.imread('your_img_path')
+crop_img = main_img_preprocess(origin_img)
+# you can save img by below code
+# io.imsave('croped.jpg', crop_img)
+```
--- skimage_clipper.py
+++ skimage_clipper.py
@@ -1,40 +1,48 @@
+# -*- coding:utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import exposure, filters, io, measure, transform
 from skimage.color import rgb2gray
 from skimage.morphology import closing, convex_hull_image, disk
 
+
+# 目标图片大小
 TARGET_H = 672
 TARGET_W = 1369
+# 预加黑边大小
+EDGE = 20
 
 
 def get_binary_img(img, kernel):
     img = rgb2gray(img)  # 转化为灰图
-    io.imshow(img)
-    plt.show()
-    grayscale = exposure.adjust_gamma(img, 3)
+    thresh = filters.threshold_otsu(img)  # otsu算法二值化
+    dst = (img >= thresh)*1.0
     selem = disk(kernel)
-    closed = closing(grayscale, selem)
-    thresh = filters.threshold_otsu(closed)
-    dst = (closed >= thresh)*1.0
-    io.imshow(dst)
-    plt.show()
-    return dst
+    closed = closing(dst, selem)  # 形态闭运算，去除票据内容
+    # 下面加上四边黑框
+    closed[:EDGE, :] = 0.
+    closed[-EDGE:, :] = 0.
+    closed[:, :EDGE] = 0.
+    closed[:, -EDGE:] = 0.
+    # io.imshow(closed)
+    # plt.show()
+    return closed
 
 
 def get_max_contour_coor(img):
     max_contour = 0
     max_coor = []
+    # 寻找最大轮廓
     for contour in measure.find_contours(img, 0):
-        coord = measure.approximate_polygon(contour, tolerance=39.5)
+        # tolerance75 寻找矩形轮廓，忽略可能存在的反光斑突起
+        coord = measure.approximate_polygon(contour, tolerance=75)
         if len(contour) > max_contour:
             max_contour = len(contour)
             max_coor = coord
-        # 可以从上面的点个数判断票据位置状态
     return len(max_coor), max_coor
 
 
-# 四边形顶点排序，[top-left,bottom-left, top-right, bottom-right ]
+# 轮廓四边形顶点排序输出用于透视变换
 def orderPoints(pts):
     rect = np.zeros((4, 2), dtype="float32")
     s = pts.sum(axis=1)
@@ -47,42 +55,38 @@ def orderPoints(pts):
 
 
 def get_crop_img(origin_img, binary_img, point, coor):
-    img = binary_img
-    if point <= 3:
-        # 如果轮廓点不大于3点，则票据必有至少一边和图片边重合，此时不需要透视拉伸转换，直接裁剪即可。
-        mask = convex_hull_image(img)
-        # Label the different objects in the image
-        label_img = measure.label(mask)
-        # Find all objects
-        regions = measure.regionprops(label_img)
-        # Find the first object (since we only have one, that's the rectangle!)
-        r = regions[0]
-        # We'll use the `bbox` property to select the bounding box
-        # # For a full list of properties, take a look at the docstring of
-        # measure.regionprops
+    shape = origin_img.shape
+    # 如果轮廓在黑边上，则减掉黑边回到原图片对应角点。
+    for co in coor:
+        if co[0] < EDGE+5:
+            co[0] = 0
+        if co[0] > shape[0] - EDGE - 5:
+            co[0] = shape[0] - 1
+        if co[1] < EDGE+5:
+            co[1] = 0
+        if co[1] > shape[1] - EDGE - 5:
+            co[1] = shape[1] - 1
 
-        r0, c0, r1, c1 = r.bbox
-        img_crop = origin_img[r0:r1, c0:c1]
-        resized = transform.resize(img_crop, (TARGET_H, TARGET_W))
-        # io.imshow(resized)
-        # plt.show()
-        return resized
-    else:
-        # 如果轮廓点为4点以上，则找轮廓点并进行透视转换
-        boxes = orderPoints(coor)
-        target = np.array(
-            [[0, 0], [0, TARGET_H], [TARGET_W, TARGET_H], [TARGET_W, 0]])
-        tform3 = transform.ProjectiveTransform()
-        tform3.estimate(target, boxes)
-        warped = transform.warp(
-            origin_img, tform3, output_shape=(TARGET_H, TARGET_W))
-        return warped
+    boxes = orderPoints(coor)
+    target = np.array(
+        [[0, 0], [0, TARGET_H], [TARGET_W, TARGET_H], [TARGET_W, 0]])
+    # 进行透视变换
+    tform3 = transform.ProjectiveTransform()
+    tform3.estimate(target, boxes)
+    warped = transform.warp(
+        origin_img, tform3, output_shape=(TARGET_H, TARGET_W))
+    return warped
 
 
-origin_img = io.imread('test.jpg')
-img = get_binary_img(origin_img, 10)
-point, coor = get_max_contour_coor(img)
-crop_img = get_crop_img(origin_img, img, point, coor)
+def main_img_preprocess(origin_img):
+    bin_img = get_binary_img(origin_img, 10)
+    point, coor = get_max_contour_coor(bin_img)
+    crop_img = get_crop_img(origin_img, bin_img, point, coor)
+    return crop_img
+
+
+# origin_img = io.imread('test5.jpg')
+# crop_img = main_img_preprocess(origin_img)
 # io.imshow(crop_img)
 # plt.show()
-io.imsave('result.jpg', crop_img)
+# io.imsave('result.jpg', crop_img)
